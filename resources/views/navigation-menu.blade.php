46009338c9af80d3bc5a23177f5f061833a250a1
w3ryt0 <robien.werito@gmail.com>
2024-06-21 15:11:03 -0600

    Sistematizacion Menu nav y user component

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -154,7 +154,6 @@
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
-
                     <x-responsive-nav-link href="{{ route('logout') }}"
                                    @click.prevent="$root.submit();">
                         {{ __('Log Out') }}
